cb1de14fde1d8ed0a659ebf77b698042147b6d22
Nicole <nuling.huang@ehsn.com.tw>
2024-05-13 14:22:55 +0800

    實例移至建構子

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -4,14 +4,20 @@ using UnitTestPlayground.Ping;
 namespace UnitTestPlayground.Tests.PingTests;
 public class NetworkServiceTests
 {
+    private readonly NetworkService _pingService;
+
+    public NetworkServiceTests()
+    {
+        //SUT: System Under Test
+        _pingService = new NetworkService();
+    }
     [Fact]
     public void NetworkService_SendPing_ReturnsString()
     {
         // Arrange
-        var networkService = new NetworkService();
 
         // Act
-        var result = networkService.SendPing();
+        var result = _pingService.SendPing();
 
         // Assert
         Assert.Equal("Success: xxx", result);
@@ -24,10 +30,9 @@ public class NetworkServiceTests
     public void NetworkService_PingTimeout_ReturnsInt(int a, int b, int expected)
     {
         // Arrange
-        var networkService = new NetworkService();
 
         // Act
-        var result = networkService.PingTimeout(a, b);
+        var result = _pingService.PingTimeout(a, b);
 
         // Assert
         //Assert.Equal(expected, result);
