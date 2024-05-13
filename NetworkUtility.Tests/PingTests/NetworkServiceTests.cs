eb743da1e3104d1f4c8a5173765f7c85b53909a7
Nicole <nuling.huang@ehsn.com.tw>
2024-05-13 14:09:00 +0800

    加入專案檔案。

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using UnitTestPlayground.Ping;
+
+namespace UnitTestPlayground.Tests.PingTests;
+public class NetworkServiceTests
+{
+    [Fact]
+    public void NetworkService_SendPing_ReturnsString()
+    {
+        // Arrange
+        var networkService = new NetworkService();
+
+        // Act
+        var result = networkService.SendPing();
+
+        // Assert
+        Assert.Equal("Success: xxx", result);
+
+    }
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(2, 3, 5)]
+    public void NetworkService_PingTimeout_ReturnsInt(int a, int b, int expected)
+    {
+        // Arrange
+        var networkService = new NetworkService();
+
+        // Act
+        var result = networkService.PingTimeout(a, b);
+
+        // Assert
+        //Assert.Equal(expected, result);
+        result.Should().Be(expected);
+    }
+}
---
+++ UnitTestPlayground/Ping/NetworkService.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTestPlayground.Ping;
+public class NetworkService
+{
+    public string SendPing()
+    {
+        return "Success: xxx";
+    }
+
+    public int PingTimeout(int a, int b)
+    {
+        return a + b;
+    }
+}
